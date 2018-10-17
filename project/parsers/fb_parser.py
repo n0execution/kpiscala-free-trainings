ceb19bac48b697537e61b93ec9ad96fc3d958837
dimasmemas <dimichkit@ukr.net>
2018-10-17 18:00:02 +0300

    added function for getting posts

--- project/parsers/fb_parser.py
+++ project/parsers/fb_parser.py
@@ -1,5 +1,6 @@
 from project import config
 from selenium import webdriver
+from bs4 import BeautifulSoup
 
 
 driver = webdriver.Chrome(config.DRIVER_PATH)
@@ -7,4 +8,11 @@ driver = webdriver.Chrome(config.DRIVER_PATH)
 
 def get_page_html():
     driver.get('https://www.facebook.com/kpiclimbing/')
+    driver.execute_script("window.scrollTo(0, 3600)")
     return driver.page_source
+
+
+def get_all_posts():
+    html = get_page_html()
+    soup = BeautifulSoup(html, 'lxml')
+    posts = soup.find('div', class_='_1xnd').find_all('div', class_='_4-u2 _4-u8')
