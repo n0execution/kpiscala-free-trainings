4f915ee892685f8dec1dd8849241f251f2a4055b
dimasmemas <dimichkit@ukr.net>
2018-10-21 20:07:26 +0300

    updated function for checking posts from Telegram

--- project/parsers/tg_parser.py
+++ project/parsers/tg_parser.py
@@ -1,9 +1,26 @@
 from telethon import TelegramClient, sync
 import config
+from helper_methods import *
+from datetime import timezone
 
 
 def check_posts(bot):
     client = TelegramClient('session_name', config.API_ID, config.API_HASH)
+    registration_date = read_registration_date()
+
     with client:
         for message in client.iter_messages('kpiclimbing', limit=20):
-            print(message.message)
+            condition1 = config.keyword in message.message
+            condition2 = get_datetime(registration_date).replace(tzinfo=timezone.utc) < message.date
+
+            if condition1 and condition2:
+                write_registration_date(message.date.strftime("%d.%m.%Y %H:%M"))
+
+                message_text = '\n' + message.message + '\n\n' + '\n'.join(config.usernames)
+                photo = open(config.PHOTO_PATH, 'rb')
+
+                message = bot.send_photo(config.CHAT_ID,
+                                         photo,
+                                         caption=message_text,
+                                         parse_mode='Markdown')
+                bot.pin_chat_message(config.CHAT_ID, message.message_id)
