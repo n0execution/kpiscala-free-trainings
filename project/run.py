3eb6bc453c93ea79f1693b1d464cec7aa8c0e7cc
dimasmemas <dimichkit@ukr.net>
2018-10-21 21:04:31 +0300

    updated run script

--- project/run.py
+++ project/run.py
@@ -1,9 +1,12 @@
-from parsers import fb_parser
+from parsers import fb_parser, tg_parser
 from time import sleep
 from bot import bot, config
 
 
 while(True):
+    print('checking telegram...')
+    tg_parser.check_posts(bot)
+    print('checking facebook...')
     fb_parser.check_posts(bot)
     print('checked')
     sleep(60)
