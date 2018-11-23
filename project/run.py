6a3a181588320b24e5e2393e3254760c15b86606
dimasmemas <dimichkit@ukr.net>
2018-11-23 10:23:08 +0200

    removed checking fb

--- project/run.py
+++ project/run.py
@@ -8,8 +8,8 @@ while(True):
     try:
         print('checking telegram...')
         tg_parser.check_posts(bot)
-        print('checking facebook...')
-        fb_parser.check_posts(bot)
+        # print('checking facebook...')
+        # fb_parser.check_posts(bot)
         print('checked')
     except Exception as e:
         print('ERROR:', e)
